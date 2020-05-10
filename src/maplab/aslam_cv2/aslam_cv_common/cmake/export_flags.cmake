239a82d2ad81118837427732841c5782691b1abb
Abdo Babukr <abdo.babukr@gmail.com>
2020-05-10 02:05:18 -0700

    arm arch neon aslam_cv_common

--- src/maplab/aslam_cv2/aslam_cv_common/cmake/export_flags.cmake
+++ src/maplab/aslam_cv2/aslam_cv_common/cmake/export_flags.cmake
@@ -4,12 +4,15 @@ if (ARCHITECTURE MATCHES "^(arm)")
   set(IS_ARM_ARCHITECTURE TRUE)
 endif()
 
-if (NOT ANDROID AND NOT IS_ARM_ARCHITECTURE)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mssse3")
-elseif (IS_ARM_ARCHITECTURE)
+
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64.*|AARCH64.*|arm64.*|ARM64.*)")
   message(STATUS "Setting ARM compilation flags.")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon -march=native")
+else()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mssse3")  
 endif()
+
+
 
 set(ENABLE_TIMING FALSE CACHE BOOL "Set to TRUE to enable timing")
 message(STATUS "Timers enabled? ${ENABLE_TIMING}")
